efb8c4fb9717b38c05410a4d599d391aaad13816
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-11-22 01:57:36 -0500

    turn off ams

--- app/controllers/site_data_controller.rb
+++ app/controllers/site_data_controller.rb
@@ -10,7 +10,39 @@ class SiteDataController < ApplicationController
   # GET /site_data/1
   # GET /site_data/1.json
   def show
+    pappysite = SiteDatum.find(1)
+      puts pappysite.inventory
 
+      mechanize = Mechanize.new
+      page = mechanize.get('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/SpiritsCatalogSearchResultView?tabSel=1&sortBy=Name&sortDir=ASC&storeId=10051&catalogId=10051&langId=-1&parent_category_rn=Spirits&newsearchlist=no&resetValue=0&searchType=Spirits&minSize=&maxSize=&promotions=&rating=&vintage=&specificType=&price=0&maxPrice=0&varitalCatIf=&region=&country=&varietal=&listSize=45&searchKey=&pageNum=1&totPages=1&level0=Spirits&level1=S_Bourbon&level2=&level3=&keyWordNew=false&VId=&TId=&CId=&RId=&PRc=&FPId=&TRId=&ProId=&isKeySearch=&SearchKeyWord=Name+or+Code')
+      
+      inventory = page.at('.tabSelected_blue').text.strip.tr('AvailableOnline)(','').to_i
+      
+      puts inventory
+
+      pappy = page.body.include?('Winkle')
+
+        if inventory == pappysite.inventory
+          puts "No Changes"
+          pappysite.inventory = inventory
+          pappysite.save
+        else
+          puts "There's a change!"
+          pappysite.inventory = inventory
+          pappysite.save
+
+        end
+
+        if pappy == true
+          puts "There's PAPPY!"
+          pappysite.pappy = true
+          pappysite.save
+
+        else
+          puts "No Pappy :("
+          pappysite.pappy = false
+          pappysite.save
+        end
 
   end
 
