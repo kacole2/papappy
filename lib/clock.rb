42bd1a4e6b6c8ef773f04974e3d418a00f4e3c99
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-11-22 01:30:12 -0500

    clock21

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,11 +1,9 @@
 require File.expand_path('../../config/boot',        __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
-require './config/boot'
-require './config/environment'
+#require './config/boot'
+#require './config/environment'
 require 'clockwork'
-require 'rake/dsl_definition'
+
 include Clockwork
 
-Rails.application.load_tasks
-
-every(30.seconds, 'Searching for Pappy...') { "rake runScrape" }
+every(30.seconds, 'Searching for Pappy...') { SiteDatum.scrape }
