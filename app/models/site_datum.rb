a57b090a2d174e6eee258b1346b1f1073490e5b2
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-11-22 01:48:42 -0500

    fix back and forth

--- app/models/site_datum.rb
+++ app/models/site_datum.rb
@@ -2,8 +2,6 @@ class SiteDatum < ActiveRecord::Base
 
 	def self.scrape
 		pappysite = SiteDatum.find(1)
-
-	  	puts pappysite.inventory
 	  	
 	    mechanize = Mechanize.new
 	    page = mechanize.get('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/SpiritsCatalogSearchResultView?tabSel=1&sortBy=Name&sortDir=ASC&storeId=10051&catalogId=10051&langId=-1&parent_category_rn=Spirits&newsearchlist=no&resetValue=0&searchType=Spirits&minSize=&maxSize=&promotions=&rating=&vintage=&specificType=&price=0&maxPrice=0&varitalCatIf=&region=&country=&varietal=&listSize=45&searchKey=&pageNum=1&totPages=1&level0=Spirits&level1=S_Bourbon&level2=&level3=&keyWordNew=false&VId=&TId=&CId=&RId=&PRc=&FPId=&TRId=&ProId=&isKeySearch=&SearchKeyWord=Name+or+Code')
@@ -13,8 +11,11 @@ class SiteDatum < ActiveRecord::Base
 	    puts inventory
 
 	    pappy = page.body.include?('Winkle')
-	      if pappysite.inventory == inventory
+
+	      if inventory == pappysite.inventory
 	        puts "No Changes"
+	        pappysite.inventory = inventory
+	        pappysite.save
 	      else
 	        puts "There's a change!"
 	        pappysite.inventory = inventory
