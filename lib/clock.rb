d850b6f411410c57190033ff93dc9941e1bf3af8
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-11-22 00:51:46 -0500

    clock 13

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../../config/boot',        __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
-#require './config/boot'
-#require './config/environment'
+require './config/boot'
+require './config/environment'
 require 'clockwork'
 
 include Clockwork
