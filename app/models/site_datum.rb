654f4ed06b5a2c130bae4c03742483f099d6cf72
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-12-06 06:49:16 -0500

    the extra 10percent

--- app/models/site_datum.rb
+++ app/models/site_datum.rb
@@ -1,46 +1,23 @@
 class SiteDatum < ActiveRecord::Base
 
 	def self.scrape
-		pappysite = SiteDatum.find(1)
-	  	
-	    mechanize = Mechanize.new
-	    page = mechanize.get('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/SpiritsCatalogSearchResultView?tabSel=1&sortBy=Name&sortDir=ASC&storeId=10051&catalogId=10051&langId=-1&parent_category_rn=Spirits&newsearchlist=no&resetValue=0&searchType=Spirits&minSize=&maxSize=&promotions=&rating=&vintage=&specificType=&price=0&maxPrice=0&varitalCatIf=&region=&country=&varietal=&listSize=45&searchKey=&pageNum=1&totPages=1&level0=Spirits&level1=S_Bourbon&level2=&level3=&keyWordNew=false&VId=&TId=&CId=&RId=&PRc=&FPId=&TRId=&ProId=&isKeySearch=&SearchKeyWord=Name+or+Code')
-	    
-	    inventory = page.at('.tabSelected_blue').text.strip.tr('AvailableOnline)(','').to_i
-	    
-	    pappyArray = ['Winkle', 'Pappy', 'Van']
-
-	    pappy = pappyArray.any? { |keyword| page.body.include? keyword }
-
 	    current_time = Time.now.in_time_zone("Eastern Time (US & Canada)")
 
 	    if current_time.hour.between?(7, 13) 
 	    	puts "it's " + current_time.strftime("%H:%M").to_s + ", lets scrape!"
-	    	
-		    if inventory == pappysite.inventory
-		    	puts "No Changes"
-		        pappysite.inventory = inventory
-		        pappysite.save
-		    else
-		        pappysite.inventory = inventory
-		        pappysite.save
-		        puts "There's a change! There are now " + pappysite.inventory.to_s + " items listed"
 
-		        #SMSEasy::Client.config['from_address'] = "PAPappy"
-		        #easy = SMSEasy::Client.new
-		        # Deliver a simple message.
-		        #easy.deliver(ENV["KENNY_NUMBER"],"at&t","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
-		        #sleep(20.seconds)
-		        #easy.deliver(ENV["BOBBY_NUMBER"],"at&t","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
-		        #easy.deliver(ENV["STEVE_NUMBER"],"verizon","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
-		        #easy.deliver(ENV["SCOTT_NUMBER"],"at&t","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
-		    end
+	    	pappysite = SiteDatum.find(1)
+	  	
+	    	mechanize = Mechanize.new
+	    	page = mechanize.get('http://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/SpiritsCatalogSearchResultView?tabSel=1&sortBy=Name&sortDir=ASC&storeId=10051&catalogId=10051&langId=-1&parent_category_rn=Spirits&newsearchlist=no&resetValue=0&searchType=Spirits&minSize=&maxSize=&promotions=&rating=&vintage=&specificType=&price=0&maxPrice=0&varitalCatIf=&region=&country=&varietal=&listSize=45&searchKey=&pageNum=1&totPages=1&level0=Spirits&level1=S_Bourbon&level2=&level3=&keyWordNew=false&VId=&TId=&CId=&RId=&PRc=&FPId=&TRId=&ProId=&isKeySearch=&SearchKeyWord=Name+or+Code')
+			inventory = page.at('.tabSelected_blue').text.strip.tr('AvailableOnline)(','').to_i
+	    
+			pappyArray = ['Winkle', 'Pappy', 'Van']
+			pappy = pappyArray.any? { |keyword| page.body.include? keyword }
 
 		    if pappy == true
 		        puts "There's PAPPY!"
-		        pappysite.pappy = true
-		        pappysite.save
-
+		   
 		        	if pappysite.textsent == false
 		        		# Override the default "from" address with config/initializers/sms-easy.rb
 				        SMSEasy::Client.config['from_address'] = "PAPappy"
@@ -160,11 +137,33 @@ class SiteDatum < ActiveRecord::Base
 						pappysite.ordersubmitted = true
 						pappysite.save
 					end
-					
+
+				pappysite.pappy = true
+		        pappysite.save
+		        
 		    else
 		        puts "No Pappy :("
 		        pappysite.pappy = false
 		        pappysite.save
+		    end
+
+		    if inventory == pappysite.inventory
+		    	puts "No Changes"
+		        pappysite.inventory = inventory
+		        pappysite.save
+		    else
+		        pappysite.inventory = inventory
+		        pappysite.save
+		        puts "There's a change! There are now " + pappysite.inventory.to_s + " items listed"
+
+		        #SMSEasy::Client.config['from_address'] = "PAPappy"
+		        #easy = SMSEasy::Client.new
+		        # Deliver a simple message.
+		        #easy.deliver(ENV["KENNY_NUMBER"],"at&t","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
+		        #sleep(20.seconds)
+		        #easy.deliver(ENV["BOBBY_NUMBER"],"at&t","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
+		        #easy.deliver(ENV["STEVE_NUMBER"],"verizon","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
+		        #easy.deliver(ENV["SCOTT_NUMBER"],"at&t","Inventory Change! There are now " + inventory.to_s + " items available. http://bit.ly/1vxVWJL")
 		    end
 
 	    else
