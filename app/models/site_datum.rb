25623d631dfad71a4621e38a55139b4363163fc5
Kendrick Coleman <kendrickcoleman@gmail.com>
2014-12-09 20:20:54 -0500

    add order for vwfrr
    not good DRY, but will fix later

--- app/models/site_datum.rb
+++ app/models/site_datum.rb
@@ -40,6 +40,8 @@ class SiteDatum < ActiveRecord::Base
 
 		    if pappy == true
 		        STDOUT.write "There's PAPPY!\n"
+		        pappysite.pappy = true
+		        pappysite.save
 		   
 		        	if pappysite.textsent == false
 		        		#Save the change in the database before clockwork runs again
@@ -153,6 +155,67 @@ class SiteDatum < ActiveRecord::Base
 							ordercomplete_text.deliver(userphone,"at&t","Pappy Order for " + userlogin.to_s + " is submitted!")
 						end
 
+						def self.order_pappyrye(userlogin, userpassword, userphone)
+							#Use Mechanize to quickly scrape and put items in the cart.
+			        		#If this was done with Watir it would have to perform a "back" function
+			        		#because it's an actual headless browser
+				        	agent1 = Mechanize.new
+				        	agent1.user_agent_alias = 'Mac Safari'
+
+				        	#Go to the login page and submit the login form
+			    			login_page = agent1.get('https://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/LogonForm?langId=-1&storeId=10051&catalogId=null')
+			    			login_form = login_page.form_with(:name => 'Logon')
+			    			login_form['logonId'] = userlogin
+							login_form['logonPassword'] = userpassword
+							login_button = login_form.button_with(:id => 'loginButton')
+							loggedin_page = login_form.submit(login_button)
+							
+							#Get the list of all the bourbons and search for keywords and add them to the cart
+							bourbon_list = agent1.get('https://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/SpiritsCatalogSearchResultView?tabSel=1&sortBy=Name&sortDir=ASC&storeId=10051&catalogId=10051&langId=-1&parent_category_rn=Wines+by+Variety&newsearchlist=yes&resetValue=&searchType=SPIRITS&minSize=&maxSize=&promotions=&rating=&vintage=&specificType=&price=&maxPrice=0&varitalCatIf=&region=&country=&varietal=&listSize=&searchKey=Winkle&pageNum=1&totPages=1&level0=&level1=&level2=&level3=&keyWordNew=true&VId=&TId=&CId=&RId=&PRc=&FPId=&TRId=&ProId=&isKeySearch=Y&SearchKeyWord=Winkle')
+							bourbon_list_array = bourbon_list.search("//table[@id='productList']")
+							bourbon_list_array.each_with_index do |list_item, index|
+								if list_item.content.include? "30744"
+									bourbon_form = bourbon_list.form_with(:name => 'OrderItemAddForma' + index.to_s)
+									bourbon_form.action = "OrderItemAdd"
+									results_page = bourbon_form.submit
+								end
+							end
+
+							#Logout with Mechanizer so we can login with Watir
+							logout_link = bourbon_list.link_with(id: 'headerLoginAnchorId')
+							logged_out_page = logout_link.click
+
+							#We need Watir to click on JS links :(
+							#This will only take a few second. We are running Headless with phantomjs
+							browser = Watir::Browser.new :phantomjs
+
+							#Login to the site
+							browser.goto "https://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/LogonForm?langId=-1&storeId=10051&catalogId=null"
+							browser.text_field(:name => 'logonId').set userlogin
+							browser.text_field(:name => 'logonPassword').set userpassword
+							browser.link(:id => 'loginButton').click
+							browser.div(:id => "accountInfo").wait_until_present
+
+							#Go to the checkout cart and click on "Quick Checkout Option"
+							browser.goto "https://www.finewineandgoodspirits.com/webapp/wcs/stores/servlet/OrderItemDisplay?langId=-1&storeId=10051&catalogId=10051&orderId=*"
+							Watir::Wait.until { browser.title == "Fine Wine & Good Spirits: Shopping cart" }
+							browser.link(:id => 'quickcheckOut').click
+
+							#Submit the Order!!
+							Watir::Wait.until { browser.title == "Fine Wine & Good Spirits: Checkout Order Review" }
+							browser.link(:id => 'submitOrder').click
+
+							#Logout for the next person
+							Watir::Wait.until { browser.title == "Fine Wine & Good Spirits: Order Confirmation" }
+							browser.link(:id => 'headerLoginAnchorId').click
+
+							STDOUT.write "Pappy Order for " + userlogin.to_s + " is submitted!\n"
+
+							SMSEasy::Client.config['from_address'] = "PAPappy"
+							ordercomplete_text = SMSEasy::Client.new
+							ordercomplete_text.deliver(userphone,"at&t","Pappy Rye Order for " + userlogin.to_s + " is submitted!")
+						end
+
 						kenny_login_1 = ENV["KENNY_ACCOUNT1_EMAIL"]
 						kenny_login_2 = ENV["KENNY_ACCOUNT2_EMAIL"]
 						kenny_pw = ENV["KENNY_ACCOUNT1_PW"]
@@ -160,10 +223,9 @@ class SiteDatum < ActiveRecord::Base
 
 						order_liquor(kenny_login_1, kenny_pw, kenny_phone, searchsite)
 						order_liquor(kenny_login_2, kenny_pw, kenny_phone, searchsite)
+						order_pappyrye(kenny_login_1, kenny_pw, kenny_phone)
+						order_pappyrye(kenny_login_2, kenny_pw, kenny_phone)
 					end
-
-				pappysite.pappy = true
-		        pappysite.save
 
 		    else
 		        STDOUT.write "No Pappy :(\n"
